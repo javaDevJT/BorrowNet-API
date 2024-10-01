b49f8096e33be0dbaf553165a669debd93cf213d
Joshua Terk <joshterk@gmail.com>
2024-10-01 16:29:45 -0400

    Update some mappings, add a login dto

--- src/main/java/com/jt/borrownetapi/controller/PreferencesController.java
+++ src/main/java/com/jt/borrownetapi/controller/PreferencesController.java
@@ -12,9 +12,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("preferences")
+@RestController
+@RequestMapping("/preferences")
 public class PreferencesController {
 
     @Autowired
--- src/main/java/com/jt/borrownetapi/controller/UserController.java
+++ src/main/java/com/jt/borrownetapi/controller/UserController.java
@@ -10,9 +10,11 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;

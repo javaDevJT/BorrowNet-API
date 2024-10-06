1c8e7b920f61549c2575c3b042d3b3ae5f67332a
Joshua Terk <joshterk@gmail.com>
2024-10-06 12:00:48 -0400

    lombokify

--- src/main/java/com/jt/borrownetapi/model/AuthResponse.java
+++ src/main/java/com/jt/borrownetapi/model/AuthResponse.java
@@ -1,37 +1,14 @@
 package com.jt.borrownetapi.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private String jwt;
     private String message;
     private Boolean status;
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
 }
